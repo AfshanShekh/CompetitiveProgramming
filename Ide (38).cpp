ae65d52de98e8faca177dc455176bc343515c498
Afshan Shekh <shekhafshan@gmail.com>
2022-07-26 14:46:52 +0530

    Add files via upload

---
+++ Ide (38).cpp
@@ -0,0 +1,76 @@
+vector<int>a;
+struct SegTree
+{
+     int N;
+     vector<int> st;
+    
+     void init(int n)
+     {
+          N = n;
+          st.resize(4 * N + 5);
+     }
+
+     
+
+     void Build(int node, int L, int R)
+     {
+          if (L == R)
+          {
+               st[node] = a[L];
+               return;
+          }
+          int M = (L + R) / 2;
+          Build(node * 2, L, M);
+          Build(node * 2 + 1, M + 1, R);
+          st[node] = max(st[node * 2], st[node * 2 + 1]);
+     }
+
+     void Update(int node, int L, int R, int pos, int val)
+     {
+          if (L == R)
+          {
+               st[node] += val;
+               return;
+          }
+          int M = (L + R) / 2;
+          if (pos <= M)
+               Update(node * 2, L, M, pos, val);
+          else
+               Update(node * 2 + 1, M + 1, R, pos, val);
+
+          st[node] = max(st[node * 2], st[node * 2 + 1]);
+     }
+
+     int Query(int node, int L, int R, int i, int j)
+     {
+          if (j < L || i > R)
+               return 0;
+          if (i <= L && R <= j)
+               return st[node];
+          int M = (L + R) / 2; 
+          return max(Query(node * 2, L, M, i, j), Query(node * 2 + 1, M + 1, R, i, j));
+          
+     }
+
+     
+
+     int query(int l, int r) { return Query(1, 1, N, l, r); }
+
+     void update(int pos, int val) { Update(1, 1, N, pos, val); }
+
+     void build() { Build(1, 1, N); }
+
+};
+
+SegTree seg;
+
+signed main()
+{
+       a.resize(n+1);
+       seg.init(n);
+       for(int i=1;i<=n;i++){
+           cin>>a[i];
+       }
+       seg.build();
+       cout<<seg.query(2,5);
+}
